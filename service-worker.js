364d95450e8d19ea6a17c5c87ee402b7a1d56d9a
tdk121212 <118935990+tdk121212@users.noreply.github.com>
2025-05-14 00:32:45 +0330

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'poker-app-v1';
+const urlsToCache = [
+  '/poker-party/',
+  '/poker-party/index.html',
+  '/poker-party/player_transactions.html',
+  '/poker-party/icons/icon-192x192.png',
+  '/poker-party/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
